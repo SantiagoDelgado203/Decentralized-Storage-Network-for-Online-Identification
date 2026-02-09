00f19f9a2ca361381076c2d473c3de10320738f2
boanwin <xnguyen2023@fau.edu>
2026-02-08 22:56:18 -0500

    Update PostgreSQL schema: modify provider table structure and adjust dump metadata

--- schema.sql
+++ schema.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict myko5Rc28bhZaRqDv4qeWNGzWMPmCZAyyrMVbNJ9ZIQk2pGu9VHN7idq91q4jjc
+\restrict mkLWr2aI8KOeAhjHhjImhTNPqCGzI04hgJafdKeqUHtqu0NBseRqoujhYrGEwY0
 
 -- Dumped from database version 18.1
 -- Dumped by pg_dump version 18.1
 
--- Started on 2026-01-31 18:04:20
+-- Started on 2026-02-08 22:51:59
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,31 +21,12 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
---
-
-CREATE SCHEMA public;
-
-
-ALTER SCHEMA public OWNER TO pg_database_owner;
-
---
--- TOC entry 5033 (class 0 OID 0)
--- Dependencies: 4
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 16388)
+-- TOC entry 219 (class 1259 OID 16440)
 -- Name: providers; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -53,14 +34,14 @@ CREATE TABLE public.providers (
     providerid uuid DEFAULT gen_random_uuid() NOT NULL,
     registeredname character varying(255) NOT NULL,
     hashedpassword character varying(255) NOT NULL,
-    salt character varying(64)
+    provideremail character varying(255) NOT NULL
 );
 
 
 ALTER TABLE public.providers OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 16396)
+-- TOC entry 220 (class 1259 OID 16449)
 -- Name: providers_providerid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -76,7 +57,7 @@ CREATE SEQUENCE public.providers_providerid_seq
 ALTER SEQUENCE public.providers_providerid_seq OWNER TO postgres;
 
 --
--- TOC entry 5034 (class 0 OID 0)
+-- TOC entry 5033 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: providers_providerid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -85,7 +66,7 @@ ALTER SEQUENCE public.providers_providerid_seq OWNED BY public.providers.provide
 
 
 --
--- TOC entry 221 (class 1259 OID 16397)
+-- TOC entry 221 (class 1259 OID 16450)
 -- Name: requests; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -102,7 +83,7 @@ CREATE TABLE public.requests (
 ALTER TABLE public.requests OWNER TO postgres;
 
 --
--- TOC entry 222 (class 1259 OID 16406)
+-- TOC entry 222 (class 1259 OID 16460)
 -- Name: requests_requestid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -118,7 +99,7 @@ CREATE SEQUENCE public.requests_requestid_seq
 ALTER SEQUENCE public.requests_requestid_seq OWNER TO postgres;
 
 --
--- TOC entry 5035 (class 0 OID 0)
+-- TOC entry 5034 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: requests_requestid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -127,22 +108,21 @@ ALTER SEQUENCE public.requests_requestid_seq OWNED BY public.requests.requestid;
 
 
 --
--- TOC entry 223 (class 1259 OID 16407)
+-- TOC entry 223 (class 1259 OID 16461)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.users (
     userid uuid DEFAULT gen_random_uuid() NOT NULL,
     email character varying(255) NOT NULL,
-    hashedpassword character varying(255) NOT NULL,
-    salt character varying(64)
+    hashedpassword character varying(255) NOT NULL
 );
 
 
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 224 (class 1259 OID 16415)
+-- TOC entry 224 (class 1259 OID 16470)
 -- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -158,7 +138,7 @@ CREATE SEQUENCE public.users_userid_seq
 ALTER SEQUENCE public.users_userid_seq OWNER TO postgres;
 
 --
--- TOC entry 5036 (class 0 OID 0)
+-- TOC entry 5035 (class 0 OID 0)
 -- Dependencies: 224
 -- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -167,7 +147,7 @@ ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- TOC entry 4870 (class 2606 OID 16483)
+-- TOC entry 4870 (class 2606 OID 16472)
 -- Name: providers providers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -176,7 +156,7 @@ ALTER TABLE ONLY public.providers
 
 
 --
--- TOC entry 4872 (class 2606 OID 16422)
+-- TOC entry 4872 (class 2606 OID 16474)
 -- Name: providers providers_registeredname_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -185,7 +165,7 @@ ALTER TABLE ONLY public.providers
 
 
 --
--- TOC entry 4874 (class 2606 OID 16506)
+-- TOC entry 4874 (class 2606 OID 16476)
 -- Name: requests requests_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -194,7 +174,7 @@ ALTER TABLE ONLY public.requests
 
 
 --
--- TOC entry 4876 (class 2606 OID 16426)
+-- TOC entry 4876 (class 2606 OID 16478)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -203,7 +183,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4878 (class 2606 OID 16461)
+-- TOC entry 4878 (class 2606 OID 16480)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -212,7 +192,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4879 (class 2606 OID 16499)
+-- TOC entry 4879 (class 2606 OID 16481)
 -- Name: requests requests_providerid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -221,7 +201,7 @@ ALTER TABLE ONLY public.requests
 
 
 --
--- TOC entry 4880 (class 2606 OID 16477)
+-- TOC entry 4880 (class 2606 OID 16486)
 -- Name: requests requests_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -229,11 +209,11 @@ ALTER TABLE ONLY public.requests
     ADD CONSTRAINT requests_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid);
 
 
--- Completed on 2026-01-31 18:04:21
+-- Completed on 2026-02-08 22:52:00
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict myko5Rc28bhZaRqDv4qeWNGzWMPmCZAyyrMVbNJ9ZIQk2pGu9VHN7idq91q4jjc
+\unrestrict mkLWr2aI8KOeAhjHhjImhTNPqCGzI04hgJafdKeqUHtqu0NBseRqoujhYrGEwY0
 
