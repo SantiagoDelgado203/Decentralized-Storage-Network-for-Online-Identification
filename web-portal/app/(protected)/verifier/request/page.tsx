83f410a1b42b9a10472347b6d08a04fc0c176c4e
Kanish <kanishdangol3702@gmail.com>
2026-02-10 17:37:12 -0500

    Fix empty verifier request page

---
+++ web-portal/app/(protected)/verifier/request/page.tsx
@@ -0,0 +1,8 @@
+export default function VerifierRequestPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Verifier Request</h1>
+      <p>Verification request functionality coming soon...</p>
+    </div>
+  )
+}
